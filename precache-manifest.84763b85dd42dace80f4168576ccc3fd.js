8387747f7263cffa6030060630a2a8d123917593
Aryan Trivedi <aryantrivedi208@gmail.com>
2023-01-15 00:57:51 +0530

    Updates

---
+++ precache-manifest.84763b85dd42dace80f4168576ccc3fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5fb47b990e291c750ce",
+    "url": "/FoodOrderReactApp/static/css/main.db951564.chunk.css"
+  },
+  {
+    "revision": "d5fb47b990e291c750ce",
+    "url": "/FoodOrderReactApp/static/js/main.d5fb47b9.chunk.js"
+  },
+  {
+    "revision": "0e0b368f72cc80a47876",
+    "url": "/FoodOrderReactApp/static/js/1.0e0b368f.chunk.js"
+  },
+  {
+    "revision": "0eeda47ede8bd056648f",
+    "url": "/FoodOrderReactApp/static/js/runtime~main.0eeda47e.js"
+  },
+  {
+    "revision": "466d63b94094ea44b20b715445a768e2",
+    "url": "/FoodOrderReactApp/static/media/food1.466d63b9.jpg"
+  },
+  {
+    "revision": "5db7b80c232088e791fbfa2752df6b97",
+    "url": "/FoodOrderReactApp/index.html"
+  }
+];
